ad59cebb34a69d6f996b7f88ddb78dbc3b615889
JustinMunoz00 <justinm880@nycstudents.net>
2024-09-27 15:50:47 +0000

    Started Code

--- Scoreboard.java
+++ Scoreboard.java
@@ -1,1 +1,11 @@
+public class Scoreboard
+{  
+    private String teamOne;
+    private String teamTwo;
+    public Scoreboard(String one, String two)
+    {
+        teamOne = one;
+        teamTwo = two;
+    }
 
+}
